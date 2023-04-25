1b4b3e13c10711ad63a301ebe29d4b455f3aa150
sohanchatterjee <sohanchatterjee20@gmail.com>
2023-04-24 22:13:06 -0400

    Update main.py
    Switched files to Housing and appropriate columns

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels.formula.api as sm
 
-data = pd.read_excel('Life Expectancy Data.xlsx')
+data = pd.read_excel('Housing.xlsx')
 print("This data is a {} type \n".format(type(data)))
 
 print("First 5 rows of data:")
@@ -15,29 +15,18 @@ print("\nThis data has {} and {}\n".format(rows, columns))
 namesOfColumns = data.columns
 print("The columns in this file are \n{}\n".format(namesOfColumns))
 
-columnPop = data['Population']
-print("Data for \'Population\' is\n{}\n".format(columnPop))
+columnPop = data['price']
+print("Data for \'price\' is\n{}\n".format(columnPop))
 
-print("First 5 rows of \'Population\'")
+print("First 5 rows of \'price\'")
 print(columnPop.head())
 
 columnPop.replace([0, 99999999999999], np.nan, inplace = True)
-print("\nMax of \'Population\' after replacing:\n{}\n".format(columnPop.max()))
+print("\nMax of \'price\' after replacing:\n{}\n".format(columnPop.max()))
 
-print("A description about \'Population\':\n{}\n".format(columnPop.describe()))
+print("A description about \'price\':\n{}\n".format(columnPop.describe()))
 
 plt.hist(columnPop.dropna(),bins=10)
-plt.xlabel('Country')
-plt.ylabel('Population')
-plt.show()
-
-# Count the number of houses with and without a waterfront
-waterfront_counts = data['Waterfront'].value_counts()
-# Create a bar plot
-plt.bar(waterfront_counts.index, waterfront_counts.values)
-# Set labels and title
-plt.xticks([0, 1], ['No Waterfront', 'Waterfront'])
-plt.ylabel('Number of Houses')
-plt.title('Number of Houses With and Without Waterfront in NY Housing Dataset')
-# Show the plot
+plt.xlabel('Price')
+plt.ylabel('Houses')
 plt.show()
