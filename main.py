46e8b87a96c08f34e0e6d901eec2827d048a4c19
sohanchatterjee <sohanchatterjee20@gmail.com>
2023-04-25 20:23:36 -0400

    Update main.py
    changed scatter plots to histograms

--- main.py
+++ main.py
@@ -9,42 +9,62 @@ print("This data is a {} type \n".format(type(Housing)))
 print("First 5 rows of data:")
 print(Housing.head())
 
+price = Housing['price']
+area = Housing['area']
+bedrooms = Housing['bedrooms']
+stories = Housing['stories']
+bathrooms = Housing['bathrooms']
+prefarea = Housing['prefarea']
+furnished = Housing['furnishingstatus']
+
 rows, columns = Housing.shape
 print("\nThis data has {} and {}\n".format(rows, columns))
 
 namesOfColumns = Housing.columns
 print("The columns in this file are \n{}\n".format(namesOfColumns))
 
-columnPop = Housing['price']
-print("Data for \'price\' is\n{}\n".format(columnPop))
-
-print("First 5 rows of \'price\'")
-print(columnPop.head())
-
-columnPop.replace([0, 99999999999999], np.nan, inplace = True)
-print("\nMax of \'price\' after replacing:\n{}\n".format(columnPop.max()))
-
-print("A description about \'price\':\n{}\n".format(columnPop.describe()))
+print("A description about \'price\':\n{}\n".format(price.describe()))
 
 #Histogram price counts
-plt.hist(columnPop.dropna(),bins=10)
+plt.hist(price.dropna(),bins=10)
 plt.xlabel('Price')
 plt.ylabel('Houses')
 plt.title('Number of Houses per Price in Housing Dataset')
 plt.show()
 
-#Bar chart bedroom counts
-# Count the number of houses for each number of beds
-bed_counts = Housing['bedrooms'].value_counts()
-# Sort the bed_counts by index
-bed_counts = bed_counts.sort_index()
-# Create a bar plot
-plt.bar(bed_counts.index, bed_counts.values)
-# Set labels and title
-plt.xlabel('Number of Beds')
-plt.ylabel('Number of Houses')
-plt.title('Number of Houses for Each Number of Beds in Housing Dataset')
-# Show the plot
+#Histogram bedroom counts
+plt.hist(bedrooms.dropna(),bins=10)
+plt.xlabel('Bedrooms')
+plt.ylabel('Houses')
+plt.title('Number of Houses per Bedrooms in Housing Dataset')
+plt.show()
+
+#Histogram bathroom counts
+plt.hist(bathrooms.dropna(),bins=10)
+plt.xlabel('Bathrooms')
+plt.ylabel('Houses')
+plt.title('Number of Houses per Bathrooms in Housing Dataset')
+plt.show()
+
+#Histogram prefarea counts
+plt.hist(prefarea.dropna(),bins=10)
+plt.xlabel('Preferred Area')
+plt.ylabel('Houses')
+plt.title('Number of Houses per Preferred Area in Housing Dataset')
+plt.show()
+
+#Histogram stories counts
+plt.hist(stories.dropna(),bins=10)
+plt.xlabel('Stories')
+plt.ylabel('Houses')
+plt.title('Number of Houses per Stories in Housing Dataset')
+plt.show()
+
+#Histogram Furnish counts
+plt.hist(furnished.dropna(),bins=10)
+plt.xlabel('Furnish Status')
+plt.ylabel('Houses')
+plt.title('Number of Houses per Furnish Status in Housing Dataset')
 plt.show()
 
 #Scatterplot Price vs. Area
@@ -55,12 +75,9 @@ plt.title('Housing Price for Living Area in Housing Dataset')
 plt.show()
 
 #Simple linear regression model
-SLR=sm.ols(formula = 'Housing.price ~ Housing.area', data = Housing).fit()
+SLR=sm.ols(formula = 'price ~ area', data = Housing).fit()
 print(SLR.summary())
 
-price = Housing['price']
-area = Housing['area']
-bedrooms = Housing['bedrooms']
 # Multi-linear regression model
 MLR=sm.ols(formula = 'price ~ area + bedrooms', data = Housing).fit()
 print(MLR.summary())
